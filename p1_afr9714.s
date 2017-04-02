f50eb0fe24f9d6a2b46fd1b7caca0d2f31805dc3
andrewr <fridout106@gmail.com>
2017-04-02 17:16:33 -0500

    Addition functionality added

--- p1_afr9714.s
+++ p1_afr9714.s
@@ -11,14 +11,17 @@
 main:
 	//First scanf
     BL  _scanf1             @ branch to scanf procedure with return
-    MOV R2, R0              @ move return value R0 to argument register R1
+    MOV R6, R0              @ move return value R0 to argument register R1
+    
+    //Operator
+    BL _getchar
+    MOV R1, R0
     
     //Second scanf
-    BL  _scanf2              @ branch to scanf procedure with return
-    MOV R3, R0              @ move return value R0 to argument register R1
-    MOV R1, R2
-    MOV R2, R3
-    BL  _printf             @ branch to print procedure with return
+    BL  _scanf2             @ branch to scanf procedure with return
+    MOV R4, R0              @ move return value R0 to argument register R1
+    
+    BL _compareA
     
     B   _exit               @ branch to exit procedure with no return
    
@@ -39,28 +42,59 @@ _printf:
     MOV PC, R4              @ return
     
 _scanf1:
-    PUSH {LR}                @ store LR since scanf call overwrites
+    PUSH {LR}               @ store LR since scanf call overwrites
     SUB SP, SP, #4          @ make room on stack
-    LDR R0, =format_str1     @ R0 contains address of format string
+    LDR R0, =format_str1    @ R0 contains address of format string
     MOV R1, SP              @ move SP to R1 to store entry on stack
     BL scanf                @ call scanf
     LDR R0, [SP]            @ load value at SP into R0
     ADD SP, SP, #4          @ restore the stack pointer
-    POP {PC}                 @ return
+    POP {PC}                @ return
     
 _scanf2:
-    PUSH {LR}                @ store LR since scanf call overwrites
+    PUSH {LR}               @ store LR since scanf call overwrites
     SUB SP, SP, #4          @ make room on stack
-    LDR R0, =format_str2     @ R0 contains address of format string
+    LDR R0, =format_str2    @ R0 contains address of format string
     MOV R1, SP              @ move SP to R1 to store entry on stack
     BL scanf                @ call scanf
     LDR R0, [SP]            @ load value at SP into R0
     ADD SP, SP, #4          @ restore the stack pointer
-    POP {PC}                 @ return
+    POP {PC}                @ return
+    
+_getchar:
+    MOV R7, #3              @ write syscall, 3
+    MOV R0, #0              @ input stream from monitor, 0
+    MOV R2, #1              @ read a single character
+    LDR R1, =read_char      @ store the character in data memory
+    SWI 0                   @ execute the system call
+    LDR R0, [R1]            @ move the character to the return register
+    AND R0, #0xFF           @ mask out all but the lowest 8 bits
+    MOV PC, LR              @ return
+    
+_compareA:
+    CMP R1, #'+'            @ compare against the constant char '@'
+    BEQ _add				@ branch to add handler
+    
+_comparS:
+    CMP R1, #'-'            @ compare against the constant char '@'
+    
+_comparM:
+    CMP R1, #'*'            @ compare against the constant char '@'
+    
+_compareMax:
+    CMP R1, #'M'            @ compare against the constant char '@'
+    
+_add:
+	MOV R5, LR              @ store LR since printf call overwrites
+    LDR R0, =printf_str     @ R0 contains formatted string address
+	ADD R1, R4, R6
+    BL printf               @ call printf
+    MOV PC, R5              @ return
+	
 
 .data
-format_str1:     .asciz      "%d"
-format_str2:     .asciz      "%d"
-prompt_str:     .asciz      "Type a number and press enter: "
-printf_str:     .asciz      "The number entered was: %d %d\n"
+format_str1:    .asciz      "%d"
+format_str2:    .asciz      "%d"
+printf_str:     .asciz      "The sum is: %d\n"
 exit_str:       .ascii      "Terminating program.\n"
+read_char:      .ascii      " "
